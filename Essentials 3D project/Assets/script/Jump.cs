7f975eabdf665b172de7f54715a2fec7c55e3fe4
ACME1301 <acme1301@outlook.com>
2024-04-21 22:24:18 -0600

    test

---
+++ Essentials 3D project/Assets/script/Jump.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public int forceConst = 5;
+
+    private bool canJump;
+    private Rigidbody selfRigidbody;
+
+    private void Start()
+    {
+        selfRigidbody = GetComponent <Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (canJump)
+        {
+            canJump = false;
+                selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+        }
+    }
+    private void Update ()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            canJump = true;
+        }
+    }
+}
